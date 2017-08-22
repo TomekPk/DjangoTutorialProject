b40c004c5a0dda66d01cbba24bb00e2c22d90565
TomekPk <src@vp.pl>
2017-08-22 15:50:38 +0200

    add new views
    description one

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     url(r'^$', views.main_view, name="main_view"),
     url(r'^contact/$', views.contact_view, name="contact_view"),
     url(r'^appinfo/$', views.appinfo_view, name="appinfo_view"),
-    #url(r'^appinfo/$', views.appinfo_view, name="appinfo_view"),
+
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail_view, name="detail_view"),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results_view, name="results_view"),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote_view, name="vote_view"),
+
 
 ]
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -10,3 +10,12 @@ def contact_view(request):
 def appinfo_view(request):
     return HttpResponse("You are at the appinfo_view")
     
+
+def detail_view(request, question_id):
+    return HttpResponse("You are at the detail_view and You are looking at question %s." % question_id)
+
+def results_view(request, question_id):
+    return HttpResponse("You are at the results_view and You are looking at question %s." % question_id)
+
+def vote_view(request, question_id):
+    return HttpResponse("You are at the vote_view and You are looking at question %s." % question_id)
